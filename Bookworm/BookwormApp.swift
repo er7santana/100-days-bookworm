e39205fd03d4fa537389e9073c18daf143e35b02
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-31 21:50:10 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
